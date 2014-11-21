9bcb370ca5281347abf8af3613ad7d13e9af33d9
adamberman <adamberman13@gmail.com>
2014-11-21 12:00:12 -0800

    create board_index view, board_index template

---
+++ app/assets/javascripts/views/board_index.js
@@ -0,0 +1,31 @@
+TrelloClone.Views.BoardIndex = Backbone.CompositeView.extend({
+	initialize: function(){
+		this.listenTo(this.collection, "sync", this.render);
+		this.listenTo(this.collection, "add", this.addBoard);
+		this.listenTo(this.collection, "remove", this.removeBoard);
+		this.collection.each(this.addBoard.bind(this));
+	},
+	template: JST['boards/board_index'],
+	addBoard: function(board){
+		var boardShow = new TrelloClone.Views.BoardIndexItem({
+			model: board
+		});
+		this.addSubview(".boards", boardShow);
+	},
+	removeBoard: function(board){
+		var boardShow = _.find(this.subviews(".boards"), function(subview){
+			return subview.model === board;
+		});
+		this.removeSubview(".boards", boardShow);
+	}
+	render: function(){
+		var that = this;
+		var content = this.template({
+			boards: this.collection
+		});
+
+		this.$el.html(content);
+		this.attachSubViews();
+		return this;
+	}
+})
