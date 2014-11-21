b501af3cd53d2c1f0139774031a203a8e8830c13
adamberman <adamberman13@gmail.com>
2014-11-21 12:09:30 -0800

    create board router, route for index, _swapViews method

---
+++ app/assets/javascripts/routers/board_router.js
@@ -0,0 +1,21 @@
+TrelloClone.Routers.BoardRouter = Backbone.Router.extend({
+	initialize: function(options){
+		this.$rootEl = options.$rootEl;
+	},
+	routes: {
+		"": "index"
+	},
+	index: function(){
+		var collection = new TrelloClone.Collections.Boards()
+		collection.fetch();
+		var view = new TrelloClone.Views.BoardIndex(collection: collection);
+		this._swapView(view);
+	},
+	_swapView: function(newView){
+		if(this._currentView){
+			this._currentView.remove();
+		}
+		this.$rootEl.html(newView.render().$el);
+		this._currentView = newView;
+	}
+})
