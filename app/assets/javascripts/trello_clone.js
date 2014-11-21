04ecf550f73d775e9329494f340b92a9e1eb7356
adamberman <adamberman13@gmail.com>
2014-11-21 12:11:54 -0800

    initialize router

--- app/assets/javascripts/trello_clone.js
+++ app/assets/javascripts/trello_clone.js
@@ -4,5 +4,7 @@ window.TrelloClone = {
   Views: {},
   Routers: {},
   initialize: function() {
+  	new TrelloClone.Routers.BoardRouter({$rootEl: $('#main')});
+  	Backbone.history.start();
   }
 };
