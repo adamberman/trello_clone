74f5f376fb5b088e7f75b2ec1f04919249c15edc
adamberman <adamberman13@gmail.com>
2014-11-21 15:15:48 -0800

    begin board_show backbone view

---
+++ app/assets/javascripts/views/board_show.js
@@ -0,0 +1,50 @@
+TrelloClone.Views.BoardShow = Backbone.CompositeView.extend({
+	initialize: function(){
+		this.listenTo(this.collection, "sync", this.render);
+		this.listenTo(this.collection, "add", this.addList);
+		this.listenTo(this.collection, "remove", this.removeList);
+		this.collection.each(this.addList.bind(this));
+	},
+	template: JST['boards/board_show'],
+	addList: function(list){
+		
+	},
+	removeList: function(list){
+
+	},
+	render: function(){
+
+	}
+})
+
+
+// TrelloClone.Views.BoardIndex = Backbone.CompositeView.extend({
+// 	initialize: function(){
+// 		this.listenTo(this.collection, "sync", this.render);
+// 		this.listenTo(this.collection, "add", this.addBoard);
+// 		this.listenTo(this.collection, "remove", this.removeBoard);
+// 		this.collection.each(this.addBoard.bind(this));
+// 	},
+// 	template: JST['boards/board_index'],
+// 	addBoard: function(board){
+// 		var boardShow = new TrelloClone.Views.BoardIndexItem({
+// 			model: board
+// 		});
+// 		this.addSubview(".boards", boardShow);
+// 	},
+// 	removeBoard: function(board){
+// 		var boardShow = _.find(this.subviews(".boards"), function(subview){
+// 			return subview.model === board;
+// 		});
+// 		this.removeSubview(".boards", boardShow);
+// 	},
+// 	render: function(){
+// 		var content = this.template({
+// 			boards: this.collection
+// 		});
+
+// 		this.$el.html(content);
+// 		this.attachSubviews();
+// 		return this;
+// 	}
+// })
