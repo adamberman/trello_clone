fd3c8f2622aa58cc0eb97de9cdd9e5e9be41d281
adamberman <adamberman13@gmail.com>
2014-11-22 17:40:23 -0800

    create board_index_new_form backbone view and template

--- app/assets/javascripts/views/board_index.js
+++ app/assets/javascripts/views/board_index.js
@@ -4,13 +4,13 @@ TrelloClone.Views.BoardIndex = Backbone.CompositeView.extend({
 		this.listenTo(this.collection, "add", this.addBoard);
 		this.listenTo(this.collection, "remove", this.removeBoard);
 		this.collection.each(this.addBoard.bind(this));
-		var newBoard = new TrelloClone.Models.Board();
-		var boardNewView = new TrelloClone.Views.BoardIndexNewItem({model: newBoard});
+		var boardNewView = new TrelloClone.Views.BoardIndexNewItem();
 		this.addSubview(".new-board", boardNewView);
 	},
 	template: JST['boards/board_index'],
 	events: {
-		"click button.board": "selectBoard"
+		"click button.board": "selectBoard",
+		"click button.new-board": "newBoard"
 	},
 	addBoard: function(board){
 		var boardShow = new TrelloClone.Views.BoardIndexItem({
@@ -29,6 +29,10 @@ TrelloClone.Views.BoardIndex = Backbone.CompositeView.extend({
 		var id = $target.data('id');
 		Backbone.history.navigate('boards/' + id, { trigger: true })
 	},
+	newBoard: function(){
+		var boardNewFormView = new TrelloClone.Views.BoardNewForm({collection: this.collection});
+		this.addSubview(".new-board", boardNewFormView);
+	}
 	render: function(){
 		var content = this.template();
 
---
+++ app/assets/javascripts/views/board_new_form.js
@@ -0,0 +1,24 @@
+TrelloClone.Views.BoardNewForm = Backbone.View.extend({
+	template: JST['boards/board_new_form'],
+	tagName: 'form',
+	events: {
+		'submit form': 'submit'
+	},
+	render: function(){
+		var content = this.template();
+		this.$el.html(renderedContent);
+	},
+	submit: function(event){
+		event.preventDefault();
+
+		var params = $(event.currentTarget).serializeJSON();
+		var newBoard = new TrelloClone.Models.Board(params["board"]);
+
+		newBoard.save({}, {
+			success: function(){
+				this.collection.add(newBoard);
+				Backbone.history.navigate('boards/' + newBoard.id, { trigger: true });
+			}.bind(this);
+		})
+	}
+})
