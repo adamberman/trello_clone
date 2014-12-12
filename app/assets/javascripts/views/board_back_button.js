352cb028672ef96df44cee0a1c08832aed98c38e
adamberman <adamberman13@gmail.com>
2014-12-12 09:43:41 -0800

    remove subviews, templates for back button, delete button

--- app/assets/javascripts/views/board_back_button.js
+++
@@ -1,9 +0,0 @@
-TrelloClone.Views.BoardBackButton = Backbone.View.extend({
-	template: JST['boards/board_back_button'],
-	render: function(){
-		var content = this.template();
-
-		this.$el.html(content);
-		return this;
-	}
-})
--- app/assets/javascripts/views/board_delete_button.js
+++
@@ -1,9 +0,0 @@
-TrelloClone.Views.BoardDeleteButton = Backbone.View.extend({
-	template: JST['boards/board_delete_button'],
-	render: function(){
-		var content = this.template();
-
-		this.$el.html(content);
-		return this;
-	}
-})
